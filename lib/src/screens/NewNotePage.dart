00b2b8b987e1013ec2cba7b3fb608e55612dd7c2
hanula1 <hania.banasiak@gmail.com>
2025-01-13 21:42:22 +0100

    NewNotePage v1 Made NewNotePage class Added Form with: Mood selection (without validation!) Emotions add button (not working yet) Activities add button (not working yet) Note section Save button (with validation check)
    TODO:
    1. Add validation for mood section (necessary for saving)
    2. Add popup for Emotions
    3. Add popup for Activities
    4. Make the page nice

--- lib/src/screens/NewNotePage.dart
+++ lib/src/screens/NewNotePage.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:moodify/src/components/CustomBlock.dart';
 
-//TODO: Create page
+/*
+TODO:
+  1. Add validation for mood section (necessary for saving)
+  2. Add popup for Emotions
+  3. Add popup for Activities
+  4. Make the page nice
+ */
 
 class NewNotePage extends StatefulWidget {
   const NewNotePage({super.key});
@@ -12,8 +19,136 @@ class NewNotePage extends StatefulWidget {
 
 class _NewNotePageState extends State<NewNotePage>
 {
+  DateTime _now = DateTime.now();
+
+  GlobalKey<FormState> _formGlobalKey = GlobalKey<FormState>();
+  int? _idMood;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+          child:
+          Column(
+            children: [
+          SizedBox(height:20),
+              CustomBlock(
+                  child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,  //spaces all children evenly in vertical axis
+
+            children: [
+              Form(
+                key: _formGlobalKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(style: TextStyle(fontSize: 45),'25th May 2025'),
+
+                    //Mood
+                    Text(style: TextStyle(fontSize: 30),'Mood:'),
+                    _moods(),
+                    SizedBox(height:20),
+
+                    //Emotions
+                    Text(style: TextStyle(fontSize: 30),'Emotions:'),
+                    Center(
+                      child: IconButton(
+                          onPressed: (){},
+                          icon: Icon(Icons.add_circle_outline_rounded, size: 50,)
+                      )
+                    ),
+                    SizedBox(height:20),
+
+                    //Activities
+                    Text(style: TextStyle(fontSize: 30),'Activities:'),
+                    Center(
+                        child: IconButton(
+                            onPressed: (){},
+                            icon: Icon(Icons.add_circle_outline_rounded, size: 50,)
+                        )
+                    ),
+                    SizedBox(height:20),
+
+                    //Note
+                    Text(style: TextStyle(fontSize: 30),'Note:'),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "How was your day?",
+                        border: OutlineInputBorder()),
+                      maxLength: 1000,
+                    ),
+
+                    SizedBox(height:20),
+
+                    //save button
+                    ElevatedButton(
+                        onPressed: (){
+                          if(_formGlobalKey.currentState!.validate()){
+                            _formGlobalKey.currentState!.save();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("Added!")),
+                            );
+                          }
+                        },
+                        child: Text("Save"),
+                        style: FilledButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0)
+                          )
+                        )
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+      ),
+              SizedBox(height:20)]
+      ),
+      )
+    );
+  }
+
+  Widget _moods() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _idMood = 0;
+            });
+          },
+          child: Icon(
+            Icons.sentiment_very_satisfied_rounded,
+            size: 50,
+            color: _idMood == 0 ? Colors.green : Colors.grey,
+          ),
+        ),
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _idMood = 1;
+            });
+          },
+          child: Icon(
+            Icons.sentiment_neutral_rounded,
+            size: 50,
+            color: _idMood == 1 ? Colors.orange : Colors.grey,
+          ),
+        ),
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              _idMood = 2;
+            });
+          },
+          child: Icon(
+            Icons.sentiment_very_dissatisfied_rounded,
+            size: 50,
+            color: _idMood == 2 ? Colors.red : Colors.grey,
+          ),
+        )
+      ],
+    );
   }
 }
