fe8d419aac37819cc9e733ef91e9e793b62839e6
Mszyfel <michal.szyfel@gmail.com>
2025-01-25 19:39:27 +0100

    Added fetching mood from db and calculating risks based on it

--- lib/src/services/TrendAnalysis.dart
+++ lib/src/services/TrendAnalysis.dart
@@ -1,9 +1,160 @@
 import 'dart:ffi';
 import 'dart:io';
 import 'dart:math';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
+import 'UserService.dart';
 
 import '../utils/Pair.dart';
 
+double abs(double number)
+{
+  if (number>=0) {
+    return number;
+  } else {
+    return -number;
+  }
+}
+class CrisisPredictionService
+{
+  CrisisPredictionService._privateConstructor();
+
+  static final CrisisPredictionService _instance = CrisisPredictionService._privateConstructor();
+
+  static CrisisPredictionService get instance => _instance;
+
+  final SupabaseClient supabase = UserService.instance.supabase;
+  
+  Future<double> calculateDailyRisk(String user_uuid) async
+  {
+    List<Pair<DateTime, List<double>>> moods = await fetchMoods(user_uuid);
+    List<Pair<DateTime, List<double>>> points = await fetchPoints(user_uuid);
+    List<Pair<DateTime, List<double>>> gathered_scores = connect(moods, points);
+    List<Pair<DateTime, double>> average_scores = calculate_averages_scores(gathered_scores);
+    List<double> risks = calculateRiskValues(average_scores);
+    return risks.last;
+  }
+  List<Pair<DateTime, double>> calculate_averages_scores(List<Pair<DateTime, List<double>>> list)
+  {
+    List<Pair<DateTime, double>> average_scores = [];
+    for(Pair<DateTime, List<double>> day in list)
+    {
+      double sum = 0;
+      for (double value in day.second)
+      {
+        sum+=value;
+      }
+      average_scores.add(Pair(day.first, sum/day.second.length));
+    }
+    return average_scores;
+  }
+
+List<Pair<DateTime, List<double>>> connect(List<Pair<DateTime, List<double>>> list1, List<Pair<DateTime, List<double>>> list2)
+{
+    List<Pair<DateTime, List<double>>> connected =[];
+    int list1_index = 0;
+    int list2_index = 0;
+    DateTime current_datetime = DateTime(2024, 1, 1);
+    if (list1[0].first.difference(list2[0].first).inDays <= 0 )
+    {
+      connected.add(list1[0]);
+      current_datetime = list1[0].first;
+      list1_index++;
+    }
+    else
+    {
+      connected.add(list2[0]);
+      current_datetime = list2[0].first;
+      list2_index++;  
+    }
+    while(list1_index < list1.length && list2_index < list2.length)
+    {
+      if(list1[list1_index].first.difference(current_datetime).inDays == 0)
+      {
+        connected[connected.length-1].second.addAll(list1[list1_index].second);
+        list1_index++;
+      }
+      else if(list2[list2_index].first.difference(current_datetime).inDays == 0)
+      {
+        connected[connected.length-1].second.addAll(list2[list2_index].second);
+        list2_index++;
+      }
+      else if(list1[list1_index].first.difference(list2[list2_index].first).inDays < 0)
+      {
+        connected.add(list1[list1_index]);
+        current_datetime = list1[list1_index].first;
+        list1_index++;
+      }
+      else if(list1[list1_index].first.difference(list2[list2_index].first).inDays >= 0)
+      {
+        connected.add(list2[list2_index]);
+        current_datetime = list2[list2_index].first;
+        list2_index++;
+      }
+    }
+    for (list1_index; list1_index < list1.length; list1_index++)
+    {
+      if(list1[list1_index].first.difference(current_datetime).inDays == 0)
+      {
+        connected[connected.length-1].second.addAll(list1[list1_index].second);
+      }
+      else
+      {
+        connected.add(list1[list1_index]);
+        current_datetime = list1[list1_index].first;
+      }
+    }
+    for (list2_index; list2_index < list2.length; list2_index++)
+    {
+      if(list2[list2_index].first.difference(current_datetime).inDays == 0)
+      {
+        connected[connected.length-1].second.addAll(list2[list2_index].second);
+      }
+      else
+      {
+        connected.add(list2[list2_index]);
+        current_datetime = list2[list2_index].first;
+      }
+    }
+    return connected;
+}
+Future<List<Pair<DateTime, List<double>>>> fetchMoods(String user_uuid) async {
+  try {
+    final response = await supabase.from('notes').select('*').eq('user_id', user_uuid);
+    print("success");
+    final List<Pair<DateTime, List<double>>> moodList = response.map((element) {
+      final DateTime createdAt = DateTime.parse(element['created_at'] as String);
+      final double mood = (element['mood'] as int).toDouble() / 4;
+      return Pair(createdAt, [mood]);
+    }).toList();
+    moodList.sort((a, b) => a.first.compareTo(b.first));
+
+    return moodList;
+  } catch (e) {
+    print("Error reading activities: $e");
+    return [];
+  }
+}
+Future<List<Pair<DateTime, List<double>>>> fetchPoints(String user_uuid) async {
+  try {
+    final response = await supabase.from('phq-9_results').select('*').eq('user_id', user_uuid);
+    print("success");
+    final List<Pair<DateTime, List<double>>> moodList = response.map((element) {
+      final DateTime createdAt = DateTime.parse(element['created_at'] as String);
+      final double points = (element['points'] as int).toDouble() / 27;
+
+      return Pair(createdAt, [points]);
+    }).toList();
+    moodList.sort((a, b) => a.first.compareTo(b.first));
+
+    return moodList;
+  } catch (e) {
+    print("Error reading activities: $e");
+    return [];
+  }
+}
+
+}
 
 List<double> calculateRiskValues(List<Pair<DateTime, double>> data,
     {int t1 = 6}) {
@@ -39,7 +190,7 @@ List<double> calculateRiskValues(List<Pair<DateTime, double>> data,
 
   List<double> risk = [];
   for (double value in normalisedDerivatives) {
-    risk.add(max(min(value / k, 1), 0));
+    risk.add(max(min(abs(value) / k, 1), 0));
   }
   return risk;
 }
@@ -109,7 +260,7 @@ List<double> calculateDerivativeOfSigma(
     int t1,
     int t2) {
       List<double> derivatives = [];
-      for (int i =0; i < t2-t1; i++)
+      for (int i = 0; i < t2-t1 - 1; i++)
       {
         double deltaSigma = data[i+1] - data[i];
         double deltaT = dataTimestamps[t1+i+1].first.difference(dataTimestamps[t1+i].first).inDays.toDouble();
@@ -119,7 +270,7 @@ List<double> calculateDerivativeOfSigma(
 }
 
 List<double> normalise(List<double> data, double mean, double sigma) {
-  List<double> normalisedData = []
+  List<double> normalisedData = [];
   for (double datum in data)
   {
     normalisedData.add((datum - mean)/ sigma);
---
+++ lib/src/utils/Pair.dart
@@ -0,0 +1,21 @@
+class Pair<T1, T2> {
+  T1 first;
+  T2 second;
+
+  Pair(this.first, this.second);
+
+  T1 getFirst() => first;
+
+  T2 getSecond() => second;
+
+  void setFirst(T1 value) {
+    first = value;
+  }
+
+  void setSecond(T2 value) {
+    second = value;
+  }
+
+  @override
+  String toString() => 'Pair($first, $second)';
+}
