3469abc6d47f8b1804e990774053ee11913af88d
Salatqa <ameti@op.pl>
2025-03-22 02:43:24 +0100

    Basic calendar, monthly and daily moods counter

--- lib/src/screens/CalendarPage.dart
+++ lib/src/screens/CalendarPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:moodify/src/components/CustomBlock.dart';
 import 'package:moodify/src/components/PageTemplate.dart';
+import 'package:table_calendar/table_calendar.dart';
+import '../services/NotesService.dart';
+import 'dart:math';
 
 // Use hero to display note
 
@@ -12,8 +15,50 @@ class CalendarPage extends StatefulWidget {
 
 }
 
-class _CalendarPageState extends State<CalendarPage>
-{
+class _CalendarPageState extends State<CalendarPage>{
+
+  final int _count = 0;
+
+  List<Map<String, dynamic>>? monthSummary;
+
+  final List<String> flowers = ['assets/very_sad.jpg', 'assets/sad.jpg', 'assets/neutral.jpg', 'assets/happy.jpg', 'assets/really_happy.jpg'];
+
+  DateTime current_day = DateTime.now();
+
+   @override
+  void initState() {
+    _loadData();
+    super.initState();
+  }
+
+  Future<void> _loadData() async {
+    final summary = await NotesService.instance.fetchWeekSummary();
+    setState(() {
+      monthSummary = summary; // Update state to trigger rebuild
+    });
+  }
+
+  void selectedDay(DateTime day, DateTime focus)
+  {
+    setState(()
+    {
+      current_day = day;
+      //selected_events = getEventsForDay(day);
+    });
+  }
+
+  //List<Event> getEventsForDay(DateTime day) {
+    //return events[day] ?? [];
+  //}
+
+  String getMoodIcon(DateTime day) {
+     
+    int number = Random().nextInt(5);
+    return flowers[number];
+    
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return PageTemplate(
@@ -25,7 +70,30 @@ class _CalendarPageState extends State<CalendarPage>
             children: [
               SizedBox(height: 20),
               Text(style: TextStyle(fontSize: 45), 'Calendar'),
-              SizedBox(height: 20),
+              SizedBox(height: 5),
+              TableCalendar(
+                headerStyle: HeaderStyle(formatButtonVisible: false, titleCentered: true),
+                rowHeight:60, 
+                focusedDay: current_day, 
+                firstDay: DateTime.utc(2025,2,30), 
+                lastDay:DateTime.utc(2125,3,30),
+                selectedDayPredicate: (day)=>isSameDay(day, current_day),
+                onDaySelected: selectedDay,
+                //eventLoader: (day) {return getEventsForDay(day);}),
+                calendarBuilders: CalendarBuilders( 
+                  defaultBuilder: (context, day, focusedDay) {
+                    String moodIconPath = getMoodIcon(day);
+                    return Center(
+                      child: Column(
+                        children: [
+                          Text(day.day.toString()),
+                          Image.asset(moodIconPath, height:40), 
+                        ],
+                        ),
+                      );
+                    }
+                ), //icons of flowers
+              ),
             ],
           ),
         ),
--- lib/src/services/NotesService.dart
+++ lib/src/services/NotesService.dart
@@ -88,4 +88,213 @@ Future<List<Map<String, dynamic>>> fetchEmotions() async {
     log("Added activities $activities to note $noteId");
     }
 
+  Future<List<Map<String, dynamic>>> fetchNotes(DateTime startDate, DateTime endDate) async {
+    String user_id = UserService.instance.user_id;
+
+    // Fetch notes belonging to users betwen startDate and endDate
+    try {
+      final response = await Supabase.instance.client
+          .from('notes')
+          .select('''
+        id,
+        created_at,
+        mood,
+        notes_emotions(emotions(emotion_name)),
+        notes_activities(activities(activity_name))
+    ''')
+          .eq('user_id', user_id)
+          .gte('created_at', startDate.toIso8601String())
+          .lte('created_at', endDate.toIso8601String());
+
+      log("Response: $response");
+      return response;
+    } catch (e) {
+      log("Error while fetching user's notes: $e");
+      return [];
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> fetchTestResults(DateTime startDate, DateTime endDate) async {
+
+    String user_id = UserService.instance.user_id;
+    try{
+      final response = await Supabase.instance.client
+        .from('phq-9_results')
+        .select('''
+        created_at,
+        points,
+        answers
+        ''')
+        .eq('user_id', user_id)
+        .gte('created_at', startDate.toIso8601String())
+        .lte('created_at', endDate.toIso8601String());
+      return response;
+    } catch(e)
+    {
+      return [];
+    }
+  }
+
+  Future<Map<String, dynamic>> _fetchAndCountWeeklyElements(
+      String? linkingTable,
+      String dataTable,
+      String columnName) async
+  {
+    try {
+    DateTime now = DateTime.now();
+
+    // Calculate the start of the week (Monday)
+    DateTime startOfWeek = now.subtract(Duration(days: now.weekday - 1));
+    startOfWeek = DateTime(startOfWeek.year, startOfWeek.month, startOfWeek.day);
+
+    DateTime endOfWeek = now;
+
+    String userId = UserService.instance.user_id;
+
+    // Fetch element count for this week
+    final response = await Supabase.instance.client
+        .from('notes')
+        .select( linkingTable != null 
+          ? '$linkingTable($dataTable($columnName))'
+          : columnName
+          )
+        .eq('user_id', userId)
+        .gte('created_at', startOfWeek.toIso8601String())
+        .lte('created_at', endOfWeek.toIso8601String());
+
+    Map<dynamic, int> countsMap = {};
+
+    // Count elements
+    for (var item in response)
+    {
+      if (linkingTable != null) {
+        var names = item[linkingTable];
+        for (var element in names)
+        {
+          String name = element[dataTable][columnName] as String;
+          countsMap[name] = (countsMap[name] ?? 0) + 1;
+        }
+      }
+      else
+      {
+        int mood = item[columnName] as int;
+        countsMap[mood] = (countsMap[mood] ?? 0) + 1;
+      }
+      
+    }
+
+    
+    List<Map<String, dynamic>> sortedList = countsMap.entries
+    .map((entry) => {'name': entry.key, 'count': entry.value})
+    .toList();
+
+    // Sort elements
+    if (linkingTable != null)
+    {
+      sortedList.sort((a, b) => (b['count'] as int).compareTo(a['count'] as int));
+    }
+    // Sort moods by value, not count
+    else{
+      sortedList.sort((a, b) => (b['name'] as int).compareTo(a['name'] as int));
+    }
+    return {'type': dataTable, 'data': sortedList};
+
+    } catch (e) {
+      log("Error fetching week summary: $e");
+      return {};
+    }
+  }
+
+
+  Future<List<Map<String, dynamic>>> fetchWeekSummary() async {
+    try {
+      return [
+        await _fetchAndCountWeeklyElements('notes_emotions','emotions','emotion_name'),
+        await _fetchAndCountWeeklyElements('notes_activities','activities','activity_name'),
+        await _fetchAndCountWeeklyElements(null,'moods', 'mood')
+      ];
+    } catch (e) {
+      log("Error fetching week summary: $e");
+      return [];
+    }
+  }
+
+  
+  Future<Map<String, dynamic>> fetchAndCountMonthMoods(DateTime date) async
+  {
+    try {
+    DateTime now = DateTime.now();
+
+    // Calculate the start of the week (Monday)
+    DateTime startOfMonth = DateTime(date.year, date.month, 1);
+
+    DateTime endOfMonth = DateTime(date.year, date.month + 1, 0); //"zero" day is last day of moth
+
+    if (endOfMonth.isAfter(now))
+    {
+      endOfMonth = now;
+    }
+
+    String userId = UserService.instance.user_id;
+
+    //Data structures
+    Map<int, int> monthMoodCount = {}; //Occurence of each of mood in month
+    Map<int, Map<int, int>> dailyMoodCount = {}; //Moods of each day
+    Map<int, int> dailyMoodAverages = {}; //Average daily mood
+
+   //Iteration by every day of month
+    for (int day = 1; day <= endOfMonth.day; day++) {
+      DateTime dayStart = DateTime(now.year, now.month, day);
+      DateTime dayEnd = dayStart.add(Duration(days: 1)).subtract(Duration(seconds: 1));
+
+      //Fetching daily moods
+      final response = await Supabase.instance.client
+          .from('notes')
+          .select('mood')
+          .eq('user_id', userId)
+          .gte('created_at', dayStart.toIso8601String())
+          .lte('created_at', dayEnd.toIso8601String());
+
+      if (response.isNotEmpty) {
+        Map<int, int> moodCounts = {}; 
+
+        for (var item in response) {
+          int mood = item['mood'] as int;
+
+          monthMoodCount[mood] = (monthMoodCount[mood] ?? 0) + 1; //counting monthly moods
+
+          moodCounts[mood] = (moodCounts[mood] ?? 0) + 1; //counting daily moods
+        }
+
+        //Calculating average mood
+        int moods_number = 0;
+        int moods_sum = 0;
+
+        moodCounts.forEach((mood, count) {
+          moods_number += count; // Count all moods
+          moods_sum += mood * count; // Sum of moods * their occurrences
+        });
+        
+        // Storing the average mood for the day
+        dailyMoodAverages[day] = moods_number > 0 ? (moods_sum / moods_number).round() : 0;
+
+        //Daily moods
+        dailyMoodCount[day] = moodCounts;
+      }
+    }
+
+    return {
+      'totalMoodCounts': monthMoodCount,
+      'dailyMoodCounts': dailyMoodCount,
+      'dailyMoodAverages': dailyMoodAverages
+    };
+    } catch (e) {
+      log("Error fetching monthly moods: $e");
+      return {
+        'totalMoodCounts': {},
+        'dailyMoodCounts': {},
+        'dailyMoodAverages': {}
+      };
+    }
+  }
 }
